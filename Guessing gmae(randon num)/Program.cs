ddc0a4dcb81bbde65127ae9bc37afc25ee17a25d
Benjamin <kodiugos@gmail.com>
2024-08-11 23:04:20 -0500

    Add project files.

---
+++ Guessing gmae(randon num)/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Guessing_gmae_randon_num_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(1, 11);
+
+            Console.WriteLine("Guess a number pls");
+            string userInput = Console.ReadLine();
+            int num;
+
+            bool isNumber = int.TryParse(userInput, out num);
+      
+
+            if(isNumber)
+            {
+                if (num == randomNumber)
+                {
+                    Console.WriteLine("You guessed correctly");
+                }
+                else
+                {
+                    Console.WriteLine("Try again!!!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("You didn\'t enter an integer");
+            }
+        }
+    }
+}
